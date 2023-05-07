009b82c690fd529cce2e3d4d6525475026d5d0cd
root <theophile.rey@wanadoo.fr>
2023-05-07 18:00:28 +0200

    Ajout de la fonction obtenir_chemin_image() -> elle permet d'obtenir le chemin de l'image sur n'importe quel PC

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
 from tkcalendar import Calendar, DateEntry
+import os
+
+
+# fonction pour obtenir le chemin d'une image (en fonction du pc et du systeme sur lequel est executer le script
+def obtenir_chemin_image(nom_image):
+
+    chemin_base = os.path.dirname(os.path.abspath(__file__))
+    dossier_images = "Images Project python"
+    chemin_image = os.path.join(chemin_base, dossier_images, nom_image)
+    return chemin_image
 
 # fonction pour afficher le calendrier ou le masquer
 def afficher_calendrier():
@@ -20,7 +30,7 @@ fenetre.geometry('400x400')
 fenetre.title('Calendrier')
 
 #création d'une icon pour le button calendrier(mettre le chemin vers le dossier contenant l'image dispo sur github)
-icon_button_calendrier = tk.PhotoImage(file="C:\\Users\\alber\\Pictures\\Images Project python\\VraiCalendar.PNG")
+icon_button_calendrier = tk.PhotoImage(file=obtenir_chemin_image("VraiCalendar.png"))
 
 # création des boutons pour ajouter des événements, trier les événements et afficher le calendrier
 
