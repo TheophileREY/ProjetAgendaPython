69cab5aee2be7ef02efdb076632622720dcd0cd0
ThéophileREY <theophile.rey@wanadoo.fr>
2023-05-13 03:30:25 +0200

    Quelques commentaires en plus et affichage des images

--- main.py
+++ main.py
@@ -3,78 +3,6 @@ import os
 import time
 
 """
-def afficher_calendrier():
-    for widget in fenetre.winfo_children():
-        if isinstance(widget, Calendar):
-            widget.destroy()
-            return
-
-    cal = Calendar(fenetre, selectmode='day', year=2023, month=1, day=1)
-    cal.pack()
-
-def masquer_recherche():
-    global recherche_frame, recherche_button_visible
-    if recherche_button_visible:
-        recherche_frame.pack_forget()
-        recherche_button_visible = False
-    else:
-        recherche_frame.pack(side='top', pady=5, padx=5, fill='x')
-        recherche_button_visible = True
-
-def chercher_date():
-    global recherche_entry, cal
-    date_str = recherche_entry.get()
-    if not date_str:
-        return
-    jour, mois, annee = map(int, date_str.split('/'))
-    cal.selection_set(annee, mois, jour)
-    cal._display_date = (annee, mois)
-    cal._update()
-
-fenetre = tk.Tk()
-fenetre.geometry('400x400')
-fenetre.title('Calendrier')
-
-icon_button_calendrier = tk.PhotoImage(file=obtenir_chemin_image("VraiCalendar.png"))
-
-calendrier_button = ttk.Button(fenetre, text='Calendrier', command=afficher_calendrier, image=icon_button_calendrier, padding=(2))
-calendrier_button.pack(side='top', pady=5, padx=5, anchor='nw')
-
-recherche_button = ttk.Button(fenetre, text='Recherche', command=masquer_recherche, padding=(2))
-recherche_button.pack(side='left', pady=5, padx=5)
-
-recherche_frame = ttk.Frame(fenetre)
-recherche_label = ttk.Label(recherche_frame, text='Rechercher une date :')
-recherche_label.pack(side='left', padx=5)
-recherche_entry = ttk.Entry(recherche_frame)
-recherche_entry.pack(side='left', padx=5)
-recherche_button_visible = False
-recherche_frame.pack_forget()
-
-recherche_button_2 = ttk.Button(recherche_frame, text='Rechercher', command=chercher_date)
-recherche_button_2.pack(side='left', padx=5)
-
-
-# fonction pour masquer la zone de recherche
-def masquer_recherche():
-    recherche_frame.pack_forget()
-    recherche_button.configure(command=afficher_recherche, text='Recherche')
-
-
-# bouton pour masquer la zone de recherche
-masquer_button = ttk.Button(fenetre, text='X', command=masquer_recherche, padding=(2))
-masquer_button.pack(side='left', pady=5, padx=5)
-
-
-def afficher_recherche():
-    recherche_frame.pack(side='top', pady=5, padx=5)
-    recherche_button.configure(command=masquer_recherche, text='Masquer')
-
-
-fenetre.mainloop()
-"""
-
-"""
 Creation de la classe Calendrier
 Elle permet de creer tout le système du calendrier. 
 Chaque jour est un bouton et lorsqu'on clique dessus, il renvoie la date permettant de l'utiliser ensuite.
@@ -94,13 +22,14 @@ class Calendrier():
             "Sunday": 7,
         }
 
-        # Création de la nouvelle fenetre du calendrier
+        # Creation de la nouvelle fenetre du calendrier
         self.fenetre_calendrier = tk.Toplevel()
         self.fenetre_calendrier.wm_resizable(0, 0)
         self.fenetre_calendrier.title("")
         self.fenetre_calendrier.config(padx=5, pady=5)
         self.fenetre_calendrier.rowconfigure(0, weight=1)
 
+        # ButtonsFrame est un widget Frame de Tkinter utilise pour organiser les boutons dans chaque fenetre
         buttonsFrame = tk.Frame(self.fenetre_calendrier)
         buttonsFrame.pack(side=tk.TOP, fill=tk.X, expand=1)
 
@@ -112,31 +41,29 @@ class Calendrier():
 
     def affichage_calendrier(self, startTime):
         # Trouver le premier jour du mois actuel
-        dayNum = int(time.strftime("%d", time.localtime(startTime)))
-        # il y a 86400sec dans 1 jour.
-        self.currentTime = startTime - (dayNum * 86400) + 86400
+        num_jour = int(time.strftime("%d", time.localtime(startTime)))
+        # il y a 86400sec dans 1 jour
+        self.currentTime = startTime - (num_jour * 86400) + 86400
         print(time.strftime("%d %B %A", time.localtime(self.currentTime)))
 
-        # Trouver le nombre de jour
         self.currentMonth = time.strftime("%m", time.localtime(self.currentTime))
         # Aucun mois ne possede moins de 27 jours, on initialise la longueur a 27
-        monthLength = 27
+        longueur_mois = 27
         for i in range(5):
             _t = self.currentTime + (86400 * (i + 27))
             print(time.strftime("%B", time.localtime(_t)))
             if time.strftime("%m", time.localtime(_t)) != self.currentMonth:
-                print(monthLength)
+                print(longueur_mois)
                 break
-            monthLength += 1
+            longueur_mois += 1
         # Detruire toutes les autres frame du calendrier qui existe.
         try:
             if (self.calendarFrame.winfo_exists() == True):
                 self.calendarFrame.destroy()
         except Exception:
-            print("Ignoring " + str(Exception) + ", first time calendarFrame is being created")
+            print("Erreur")
 
-        self.calendarFrame = tk.LabelFrame(self.fenetre_calendrier,
-                                           text=time.strftime("%B %Y", time.localtime(self.currentTime)))
+        self.calendarFrame = tk.LabelFrame(self.fenetre_calendrier, text=time.strftime("%B %Y", time.localtime(self.currentTime)))
         self.calendarFrame.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 
         # Placer les jours en tete du calendrier
@@ -155,7 +82,7 @@ class Calendrier():
 
         # Creation des boutons pour chaque jour
         row = 1
-        for i in range(monthLength):
+        for i in range(longueur_mois):
             _d = time.localtime(self.currentTime + (86400 * i))
             _day = time.strftime("%A", _d)
 
@@ -197,7 +124,9 @@ class Affichage():
         self.fenetre = tk.Tk()
         self.fenetre.geometry('400x400')
         self.fenetre.title('Agenda')
-        tk.Button(self.fenetre, text="Calendrier", command=lambda: Calendrier(time.time())).pack(side=tk.TOP)
+        self.icone_calendrier = tk.PhotoImage(file=obtenir_chemin_image("VraiCalendar.png"))
+        # print(obtenir_chemin_image("VraiCalendar.png"))
+        tk.Button(self.fenetre, image=self.icone_calendrier, command=lambda: Calendrier(time.time())).pack(side=tk.TOP, anchor="nw")
 
 
 def obtenir_chemin_image(nom_image):
@@ -206,6 +135,6 @@ def obtenir_chemin_image(nom_image):
     chemin_image = os.path.join(chemin_base, dossier_images, nom_image)
     return chemin_image
 
-
+# Affichage
 fenetre = Affichage()
 fenetre.fenetre.mainloop()
